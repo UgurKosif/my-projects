698a335383ea2408345b8622d6ccbc243e84746d
UgurKosif <ugkosif@gmail.com>
2023-01-27 22:36:27 +0300

    27.01 de gönderilen

--- aws/projects/Project-001-Roman-Numerals-Converter/app.py
+++ aws/projects/Project-001-Roman-Numerals-Converter/app.py
@@ -27,4 +27,4 @@ def main_post():
 
 if __name__ == '__main__':
     # app.run(debug=True)
-    app.run(host='0.0.0.0', port=80)   # sadece "localhost" yaz "localhost:5000" yazınca çıkmıyor.
+    app.run(host='0.0.0.0', port=80)
